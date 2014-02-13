6dd748c15a1b3ffa82ac658f1677660728942cab
shogunsea <shogunsea@foxmail.com>
2014-02-12 19:12:48 -0500

    add Two stack as Queue

---
+++ Stack/TwoStacksQueue.java
@@ -0,0 +1,48 @@
+package Own;
+import java.util.Stack;
+public class Queue<T> {
+	Stack<T> newStack;
+	Stack<T> oldStack;
+
+	public Queue(){
+		newStack = new Stack<T>();
+		oldStack = new Stack<T>();
+	}
+
+	public void enqueue(T value){
+		newStack.push(value);
+	}
+
+	public T dequeue(){
+		shift();
+		oldStack.pop();
+	}
+
+	public boolean isEmpty(){
+		return newStack.size()+oldStack.size()==0;
+	}
+
+	public int size(){
+		return newStack.size()+oldStack.size();
+	}
+
+	private void shift(){
+		if(oldStack.isEmpty){
+			while(!newStack.isEmpty()){
+				oldStack.push(newStack.pop());
+			}
+		}
+	}
+
+	public static void main(String[] args){
+		Queue<Integer> ins = new Queue<Integer>();
+		for(int i = 0; i<100; i++){
+			ins.enqueue(i);
+		}
+		while(!ins.isEmpty){
+			System.out.println(ins.dequeue());
+		}
+
+
+	}
+}
